41bf82dbb9d1a5333115c81194e51691f2201b9f
amanda <amandaagostinhob@gmail.com>
2022-02-22 21:47:47 -0300

    aula 3

---
+++ aula3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insíra seu primeiro nome :");
+            string primeiroNome = Console.ReadLine();
+            Console.WriteLine("insíra seu ultimo nome :");
+            string ultimoNome = Console.ReadLine();
+            Console.WriteLine("informe sua idade ;");
+            string idade = Console.ReadLine();
+            Console.WriteLine("informe sua cidade :");
+            string cidade = Console.ReadLine();
+            Console.WriteLine("nome completo : " + primeiroNome + " " + ultimoNome);
+            Console.WriteLine("cidade: " + cidade+ " " + "idade: " + idade);
+            Console.ReadKey();
+        }
+    }
+}
