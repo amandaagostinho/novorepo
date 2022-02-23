a7b89baf2bfe22af776aa813f6391d57c9d30ae5
amanda <amandaagostinhob@gmail.com>
2022-02-22 22:03:17 -0300

    aula 4

---
+++ aula4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int digite = Convert.ToInt32(Console.ReadLine());
+            if(digite % 2 == 0)
+            {
+                Console.WriteLine("O número é par");
+            }
+            else
+            {
+                Console.WriteLine("O número é impar");
+            }
+        }   
+
+           
+
+             
+    }
+}
